4f94e73c1383e81479e54098d5ecc8b722c79692
Misty <artemoutside@gmail.com>
2023-04-15 22:24:09 +0300

    vue template

---
+++ src/components/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="App">
+    <Links/>
+    <Article/>
+    <Shift/>
+  </div>
+</template>
+<script>
+import Links from './Links';
+import Shift  from './Shift';
+import Article from './Article'
+import {defineComponent} from "vue";
+ export default defineComponent({
+   name: "App",
+   components: { Links, Article, Shift },
+   data() {
+     return {
+
+     }
+   }
+ })
+</script>
+
+<style lang="scss">
+@import '../scss/fonts.scss';
+ * {
+   padding: 0;
+   margin: 0;
+ }
+ a {
+   text-decoration: none;
+   color: #000000;
+ }
+ body {
+   background: #F0F0F0;
+   font-family: "Suisse Intl", sans-serif;
+ }
+ .App {
+   position: relative;
+ }
+</style>
---
+++ src/components/Article.vue
@@ -0,0 +1,10 @@
+<template>
+  <div></div>
+</template>
+<script>
+ export default {
+   name: "Article",
+
+ }
+</script>
+<style lang="scss"></style>
